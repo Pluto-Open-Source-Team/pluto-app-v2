2c6bbd07f7bde360fdf18f922f1be8d509f465dc
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2024-03-05 13:55:05 +0100

    added a placeholder if Gg not connected

--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import Dashboard from '@/layouts/Dashboard';
 import OrgUnitsTree from '@/views/DashboardView/components/OrgUnitsTree';
+import { useAuth } from '@/hooks/use-auth';
 
 const DashboardView = (): JSX.Element => {
+  const auth = useAuth();
+
   const orgChart = {
     name: 'Test Root',
     children: [
@@ -48,6 +52,9 @@ const DashboardView = (): JSX.Element => {
     <Box sx={{ overflowX: 'hidden' }}>
       <Dashboard>
         <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
           width={1}
           height={1}
           minHeight={800}
@@ -58,12 +65,41 @@ const DashboardView = (): JSX.Element => {
             overflow: 'auto',
           }}
         >
-          <div
-            id="treeWrapper"
-            style={{ width: '100em', height: '50em' }}
-          >
-            <OrgUnitsTree data={orgChart} />
-          </div>
+          {auth && auth.isAuthenticated ? (
+            <div
+              id="treeWrapper"
+              style={{ width: '100em', height: '50em' }}
+            >
+              <OrgUnitsTree data={orgChart} />
+            </div>
+          ) : (
+            <Box
+              width={1}
+              height={1}
+              data-aos={'fade-up'}
+            >
+              <Box
+                display={'flex'}
+                flexDirection={'column'}
+                alignItems={'center'}
+              >
+                <Typography
+                  variant={'h6'}
+                  gutterBottom
+                  sx={{ fontWeight: 500 }}
+                  align={'center'}
+                >
+                  You are not connected with your Google Admin
+                </Typography>
+                <Typography
+                  align={'center'}
+                  color="text.secondary"
+                >
+                  Login with your Google Admin Account to continue
+                </Typography>
+              </Box>
+            </Box>
+          )}
         </Box>
       </Dashboard>
     </Box>

827f4b18975277a01a6576da5b0c908d19cf6822
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2024-03-11 13:38:34 +0100

    fix: refacto svg icons

--- src/layouts/Dashboard/components/Sidebar/components/SidebarNav/SidebarNav.tsx
+++ src/layouts/Dashboard/components/Sidebar/components/SidebarNav/SidebarNav.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { GoogleIcon } from '@/assets/svg-icons/GoogleIcon';
+import { GithubIcon } from '@/assets/svg-icons/GithubIcon';
+import { DashboardIcon } from '@/assets/svg-icons/DashboardIcon';
+import { ShareIcon } from '@/assets/svg-icons/ShareIcon';
+import { SettingsIcon } from '@/assets/svg-icons/SettingsIcon';
 import { useAuth } from '@/hooks/use-auth';
 import Chip from '@mui/material/Chip';
 import Link from 'next/link';
@@ -10,114 +14,22 @@ const sideBarItems = [
   {
     title: 'Home',
     href: '/dashboard',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-home"
-      >
-        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-        <polyline points="9 22 9 12 15 12 15 22" />
-      </svg>
-    ),
+    icon: <DashboardIcon />,
   },
   {
     title: 'Contribute',
     href: '/',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-github"
-      >
-        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-      </svg>
-    ),
+    icon: <GithubIcon />,
   },
   {
     title: 'Share',
     href: '/',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-share-2"
-      >
-        <circle
-          cx="18"
-          cy="5"
-          r="3"
-        />
-        <circle
-          cx="6"
-          cy="12"
-          r="3"
-        />
-        <circle
-          cx="18"
-          cy="19"
-          r="3"
-        />
-        <line
-          x1="8.59"
-          y1="13.51"
-          x2="15.42"
-          y2="17.49"
-        />
-        <line
-          x1="15.41"
-          y1="6.51"
-          x2="8.59"
-          y2="10.49"
-        />
-      </svg>
-    ),
+    icon: <ShareIcon />,
   },
   {
     title: 'Settings',
     href: '/',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-settings"
-      >
-        <circle
-          cx="12"
-          cy="12"
-          r="3"
-        />
-        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
-      </svg>
-    ),
+    icon: <SettingsIcon />,
   },
 ];
 
